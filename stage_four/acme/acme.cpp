2985f1f6357d0d6ceb10e97531631a8434117fb3
markgrin <lew.ka@hotmail.com>
2017-09-26 22:01:28 +0300

    Оптимизация acme

--- stage_four/acme/acme.cpp
+++ stage_four/acme/acme.cpp
@@ -18,10 +18,7 @@ namespace
     double distance (int x1, int y1, int z1,
                      int x2, int y2, int z2)
     {
-        return sqrt((static_cast<double>(x1)-x2)*(static_cast<double>(x1)-x2) +
-                    (static_cast<double>(y1)-y2)*(static_cast<double>(y1)-y2) +
-                    (static_cast<double>(z1)-z2)*(static_cast<double>(z1)-z2));
-
+        return hypot( hypot(x1 - x2, y1 - y2), z1 - z2);
     }
 }
 
@@ -31,12 +28,24 @@ void bomb (mdArray::Array& array, const std::size_t* dimensions, std::size_t rad
     int bomb_y = uniform_random(0, dimensions[1]);
     int bomb_z = uniform_random(0, dimensions[2]);
 
+    std::size_t start_x = bomb_x - static_cast<int>(radius) > 0 ? bomb_x - radius : 0;
+    std::size_t start_y = bomb_y - static_cast<int>(radius) > 0 ? bomb_y - radius : 0;
+    std::size_t start_z = bomb_z - static_cast<int>(radius) > 0 ? bomb_z - radius : 0;
+
+    std::size_t stop_x = bomb_x + radius;
+    std::size_t stop_y = bomb_y + radius;
+    std::size_t stop_z = bomb_z + radius;
+
+    stop_x = stop_x > dimensions[0] ? dimensions[0] : stop_x;
+    stop_y = stop_y > dimensions[1] ? dimensions[1] : stop_y;
+    stop_z = stop_z > dimensions[2] ? dimensions[2] : stop_z;
+
     std::size_t coord[3] = {0, 0, 0};
-    for (std::size_t x = 0; x < dimensions[0]; x++)
+    for (std::size_t x = start_x; x < stop_x; x++)
     {
-        for (std::size_t y = 0; y < dimensions[1]; y++)
+        for (std::size_t y = start_y; y < stop_y; y++)
         {
-            for (std::size_t z = 0; z < dimensions[2]; z++)
+            for (std::size_t z = start_z; z < stop_z; z++)
             {
                 if ( distance (bomb_x, bomb_y, bomb_z, x, y, z) <= radius )
                 {
